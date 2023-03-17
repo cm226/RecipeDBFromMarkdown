53415299cd90a772950c064c0f04118d5bfa9043
Unknown <c.matear@gmail.com>
2023-03-17 12:12:56 +0000

    adding check boxes

--- packages/frontend/src/components/ShoppingList.tsx
+++ packages/frontend/src/components/ShoppingList.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
@@ -13,46 +15,95 @@ export interface SelectedRecipesProps {
   onDelete: (r: Ingredient) => void;
 }
 
-export function ShoppingList(props: SelectedRecipesProps) {
-  const handleToggle = (value: string) => () => {};
+export interface SelectableListItemProps {
+  value: Ingredient;
+  checked: boolean;
+  check: () => void;
+  onDelete: (r: Ingredient) => void;
+}
 
+export function SelectableListItem(props: SelectableListItemProps) {
+  const value = props.value;
+  const labelId = `checkbox-list-label-${value.name}`;
+  return (
+    <ListItem key={`${value.name}_${value.recipe}`} disablePadding>
+      <ListItemButton role={undefined} onClick={() => props.check()} dense>
+        <ListItemIcon>
+          <Checkbox
+            edge="start"
+            checked={props.checked}
+            tabIndex={-1}
+            disableRipple
+            inputProps={{ "aria-labelledby": labelId }}
+          />{" "}
+        </ListItemIcon>{" "}
+        <ListItemText
+          id={labelId}
+          primary={`${value.name}`}
+          secondary={`${value.recipe}`}
+        />
+        <IconButton
+          aria-label="delete"
+          size="large"
+          onClick={() => props.onDelete(value)}
+        >
+          <DeleteIcon fontSize="inherit" />
+        </IconButton>
+      </ListItemButton>
+    </ListItem>
+  );
+}
+
+export interface ListItemsProps {
+  checked: Map<string, boolean>;
+  showChecked: boolean;
+  shoppingList: Ingredient[];
+  onDelete: (r: Ingredient) => void;
+  onCheck: (name: string) => void;
+}
+
+export function ListItems(props: ListItemsProps) {
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {props.shoppingList.map((value) => {
-        const labelId = `checkbox-list-label-${value.name}`;
-
-        return (
-          <ListItem key={`${value.name}_${value.recipe}`} disablePadding>
-            <ListItemButton
-              role={undefined}
-              onClick={handleToggle(value.name)}
-              dense
-            >
-              <ListItemIcon>
-                <Checkbox
-                  edge="start"
-                  checked={false}
-                  tabIndex={-1}
-                  disableRipple
-                  inputProps={{ "aria-labelledby": labelId }}
-                />{" "}
-              </ListItemIcon>{" "}
-              <ListItemText
-                id={labelId}
-                primary={`${value.name}`}
-                secondary={`${value.recipe}`}
-              />
-              <IconButton
-                aria-label="delete"
-                size="large"
-                onClick={() => props.onDelete(value)}
-              >
-                <DeleteIcon fontSize="inherit" />
-              </IconButton>
-            </ListItemButton>
-          </ListItem>
-        );
+        if ((props.checked.get(value.name) ?? false) === props.showChecked)
+          return (
+            <SelectableListItem
+              value={value}
+              onDelete={props.onDelete}
+              checked={props.checked.get(value.name) ?? false}
+              check={() => props.onCheck(value.name)}
+            ></SelectableListItem>
+          );
       })}
     </List>
+  );
+}
+
+export function ShoppingList(props: SelectedRecipesProps) {
+  const [checked, setChecked] = React.useState(new Map<string, boolean>());
+  return (
+    <div>
+      <ListItems
+        checked={checked}
+        shoppingList={props.shoppingList}
+        showChecked={false}
+        onDelete={props.onDelete}
+        onCheck={(name) => {
+          checked.set(name, !(checked.get(name) ?? false));
+          setChecked(new Map(checked));
+        }}
+      ></ListItems>
+      <ListItems
+        checked={checked}
+        shoppingList={props.shoppingList}
+        showChecked={true}
+        onDelete={props.onDelete}
+        onCheck={(name) => {
+          checked.set(name, !(checked.get(name) ?? false));
+          setChecked(new Map(checked));
+        }}
+      ></ListItems>
+    </div>
   );
 }
